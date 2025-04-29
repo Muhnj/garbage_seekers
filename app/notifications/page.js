b783db29ff21fcc9529d130da2851ee810634e36
Munir Mujuzi <munirumujuzi@gmail.com>
2025-04-30 01:51:17 +0300

    next js application

---
+++ app/notifications/page.js
@@ -0,0 +1,7 @@
+export default function NotificationsPage(){
+    return(
+      <div> <p>Please Notifications are found here</p></div>
+      
+    )
+  }
+  
---
+++ app/settings/sound/page.js
@@ -0,0 +1,7 @@
+export default function SoundPage(){
+    return(
+      <div> <p>sound dialog</p></div>
+      
+    )
+  }
+  
